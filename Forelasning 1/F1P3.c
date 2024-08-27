20b3755771b49338e7192a7686713ee5f2c272bb
Alecod3 <alendkhalaf@hotmail.com>
2024-08-27 23:19:49 +0200

    Forelasning1

--- Forelasning 1/F1P3.c
+++ Forelasning 1/F1P3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#define M_PI           3.14159265358979323846 // Behövs inte? 
 
 int main(void)
 {
@@ -8,7 +9,7 @@ int main(void)
     printf("Radie? ");
     scanf("%f", &r);
 
-    v = (4.0 / 3.0) * M_PI * r * r * r;
+    v = (4.0 / 3.0) *  M_PI * r * r * r;
     
     printf("Volymen av sfaren med radien %.2f m blir %.2f kubikmeter", r, v);
     return 0;
