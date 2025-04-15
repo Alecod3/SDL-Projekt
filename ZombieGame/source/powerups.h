623d5f49765c5746f06b690377bf5fac4954f323
Alecod3 <alendkhalaf@hotmail.com>
2025-04-15 14:54:08 +0200

    Infinite Game Loop
    Mobs and Powerups spawn while playing and the game goes on infinitely until u die.

--- ZombieGame/source/powerups.h
+++ ZombieGame/source/powerups.h
@@ -16,6 +16,13 @@ typedef enum {
 } PowerupType;
 
 typedef struct {
+    bool speed_active;
+    Uint32 speed_start_time;
+    bool damage_active;
+    Uint32 damage_start_time;
+} ActiveEffects;
+
+typedef struct {
     PowerupType type;
     SDL_Rect rect;
     bool active;
@@ -26,9 +33,10 @@ typedef struct {
 
 // Funktioner
 Powerup create_powerup(PowerupType type, int x, int y);
-void check_powerup_collision(Powerup* p, SDL_Rect player_rect, int* lives, int* player_speed, int* player_damage, Uint32 current_time);
-void update_powerup_effect(Powerup* p, int* player_speed, int* player_damage, Uint32 current_time);
+void check_powerup_collision(Powerup* p, SDL_Rect player_rect, int* lives, int* player_speed, int* player_damage, Uint32 current_time, ActiveEffects* effects);
+void update_powerup_effect(Powerup* powerups, int index, int* player_speed, int* player_damage, Uint32 current_time);
 void draw_powerup(SDL_Renderer* renderer, Powerup* p);
+void update_effects(ActiveEffects* effects, int* player_speed, int* player_damage, Uint32 current_time);
 
 extern SDL_Texture* tex_extralife;
 extern SDL_Texture* tex_extraspeed;
