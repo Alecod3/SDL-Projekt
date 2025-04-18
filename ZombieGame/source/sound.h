998743d1d021d3d70333ee7ba8618908cb0abe2e
yasminjalood <yjalood@ug.kth.se>
2025-04-19 00:29:00 +0200

    funktionsdeklarationer för ljudeffekter

--- ZombieGame/source/sound.h
+++ ZombieGame/source/sound.h
@@ -6,5 +6,10 @@
 void init_sound();
 void play_music(const char *filename);
 void cleanup_sound();
+void play_shoot_sound();
+void play_extralife_sound();
+void play_speed_sound();
+void play_freeze_sound();
+void play_damage_sound();
 
 #endif
